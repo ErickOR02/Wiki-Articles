513efa979a308d7cdc9fb699e8c8a7d71d2c00e7
ffigueroa0212 <46831393+ffigueroa0212@users.noreply.github.com>
2025-04-21 11:49:53 -0700

    Cleaning repo

--- src/pages/AlertasEjemplo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AlertasEjemplo() {
-  return (
-    <div>AlertasEjemplo</div>
-  )
-}
-
-export default AlertasEjemplo
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path to match your project structure
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
